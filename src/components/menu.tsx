f781a6dc48d67cd63311b7ed38aa5f0f8e83f5b5
Phoenix <ayomikuntemitope.araoye@gmail.com>
2024-01-24 14:50:47 +0200

    build error

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -33,35 +33,34 @@ function CardMenu(props: { transparent?: boolean; vertical?: boolean }) {
       }
       animation={"origin-top-right transition-all duration-300 ease-in-out"}
       classNames={`${transparent ? "top-8" : "top-11"} right-0 w-max`}
-      children={
-        <div className="z-50 w-max rounded-xl bg-white py-3 px-4 text-sm shadow-xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-          <p className="hover:text-black flex cursor-pointer items-center gap-2 text-gray-600 hover:font-medium">
-            <span>
-              <AiOutlineUser />
-            </span>
-            weekly
-          </p>
-          <p className="hover:text-black mt-2 flex cursor-pointer items-center gap-2 pt-1 text-gray-600 hover:font-medium">
-            <span>
-              <AiOutlineShop />
-            </span>
-            daily
-          </p>
-          <p className="hover:text-black mt-2 flex cursor-pointer items-center gap-2 pt-1 text-gray-600 hover:font-medium">
-            <span>
-              <TiLightbulb />
-            </span>
-            monthly
-          </p>
-          <p className="hover:text-black mt-2 flex cursor-pointer items-center gap-2 pt-1 text-gray-600 hover:font-medium">
-            <span>
-              <FiSettings />
-            </span>
-            yearly
-          </p>
-        </div>
-      }
-    />
+    >
+      <div className="z-50 w-max rounded-xl bg-white py-3 px-4 text-sm shadow-xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+        <p className="hover:text-black flex cursor-pointer items-center gap-2 text-gray-600 hover:font-medium">
+          <span>
+            <AiOutlineUser />
+          </span>
+          weekly
+        </p>
+        <p className="hover:text-black mt-2 flex cursor-pointer items-center gap-2 pt-1 text-gray-600 hover:font-medium">
+          <span>
+            <AiOutlineShop />
+          </span>
+          daily
+        </p>
+        <p className="hover:text-black mt-2 flex cursor-pointer items-center gap-2 pt-1 text-gray-600 hover:font-medium">
+          <span>
+            <TiLightbulb />
+          </span>
+          monthly
+        </p>
+        <p className="hover:text-black mt-2 flex cursor-pointer items-center gap-2 pt-1 text-gray-600 hover:font-medium">
+          <span>
+            <FiSettings />
+          </span>
+          yearly
+        </p>
+      </div>
+    </Dropdown>
   );
 }
 
