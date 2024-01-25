ceebfa747fe54d878e4eb3aa14a2b9274e36694b
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-01-25 20:57:43 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
   return (
     <header
       className={cn(
-        "lg:px-9 px-3 border-b border-gray-200 dark:border-primary-light h-[50px] sm:h-[70px] md:h-[89px] flex items-center justify-between fixed md:relative max-md:top-0 max-md:left-0 max-md:z-[99] select-none bg-white/80 dark:!bg-navy-800 backdrop-blur-lg w-full"
+        "lg:px-9 px-3 border-b border-gray-200 dark:border-primary-light h-[50px] sm:h-[70px] md:h-[89px] flex items-center justify-between fixed  max-md:top-0 max-md:left-0 max-md:z-[99] select-none bg-white/80 dark:!bg-navy-800 backdrop-blur-lg w-full"
       )}
     >
       <div className="md:flex hidden gap-x-2 sm:gap-x-4 items-center">
