c3e68171095b1d24aa3e9be725a651fa7a953f72
Phoenix <ayomikuntemitope.araoye@gmail.com>
2024-01-25 23:14:24 +0200

    little chages

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
   return (
     <section
       aria-labelledby="heading"
-      className="px-4 py-8 sm:px-[31px] sm:py-[34px] bg-white dark:!bg-navy-800 dark:text-white"
+      className="px-4 py-8 sm:px-[31px] sm:py-[34px] bg-white dark:!bg-navy-800 dark:text-white w-full"
     >
       <div className="flex items-center gap-[9px] mb-6 sm:mb-[33px]">
         <h1 id="heading" className="font-extrabold text-xl sm:text-2xl">
