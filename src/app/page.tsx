00200873c134aeac80fb729fbbe46abfc1025ff5
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-01-26 01:10:29 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const Home = () => {
     <section className="">
       <div className="flex items-center w-full gap-5 justify-between">
         <SalesTrend />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-2 ml-2 md: ml-0 md:pl-0 mb-5 md:mb-0 gap-5 self-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-2 ml-2 md:ml-0 md:pl-0 mb-5 md:mb-0 gap-5 self-center">
           <Order2 />
           <TotalRefundComponent />
           <SalesStatistics />
