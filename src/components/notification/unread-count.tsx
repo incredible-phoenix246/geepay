db253c409ac173b8f95f21539bdcdda27a6575d8
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-01-26 01:34:56 +0100

    Update unread-count.tsx

--- src/components/notification/unread-count.tsx
+++ src/components/notification/unread-count.tsx
@@ -1,6 +1,6 @@
 export default function UnreadCount({ count }: { count: number }) {
   return (
-    <div className="text-white bg-!bg-navy-800  dark:!text-navy-800 dark:bg-white font-extrabold px-[11px] pt-0.5 pb-[3px] rounded-md leading-tight">
+    <div className="text-white !bg--navy-800  dark:!text-navy-800 dark:bg-white font-extrabold px-[11px] pt-0.5 pb-[3px] rounded-md leading-tight">
       <span className="select-none">{count}</span>
     </div>
   );
