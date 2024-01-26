d77e5436458a98371c5da0fac902b6b0e4685ea6
James Ayomikun Araoye <ayomikuntemitope.araoye@gmail.com>
2024-01-26 11:51:57 +0100

    Update topplatform.tsx

--- src/components/topplatform.tsx
+++ src/components/topplatform.tsx
@@ -8,7 +8,10 @@ const TopPlat = () => {
         <h2 className="text-semibold text-xl leading-6 text-black dark:text-[#34CAA5]">
           Book Bazaar
         </h2>
+    <div className="lg:flex hidden">
         <Progress width="w-[400px]" value={80} />
+  </div>
+        <Progress width="w-[300px]" value={80} />
         <div className="flex justify-between items-center">
           <p className="text-neutral-600 text-lg leading-6">$2,500,000</p>
           <p className="text-neutral-600 text-lg leading-6">+15%</p>
